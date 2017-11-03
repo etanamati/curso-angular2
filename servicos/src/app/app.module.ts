9f88164fba5f4c708402b565f52077677af6107c
Elton Tanamati <elton.tanamati@elotech.localnet>
2017-11-03 12:15:55 -0200

    Finalizado modulo services

--- servicos/src/app/app.module.ts
+++ servicos/src/app/app.module.ts
@@ -1,20 +1,23 @@
+import { LogService } from './shared/log.service';
+import { CursosModule } from './cursos/cursos.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { CursosComponent } from './cursos/cursos.component';
 
 import { CursosService } from './cursos/cursos.service';
+import { CriarCursoModule } from './criar-curso/criar-curso.module';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    CursosComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CriarCursoModule,
+    CursosModule
   ],
-  providers: [CursosService],
+  providers: [LogService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- servicos/src/app/cursos/cursos.component.ts
+++ servicos/src/app/cursos/cursos.component.ts
@@ -5,7 +5,8 @@ import { CursosService } from './cursos.service';
 @Component({
   selector: 'app-cursos',
   templateUrl: './cursos.component.html',
-  styleUrls: ['./cursos.component.css']
+  styleUrls: ['./cursos.component.css'],
+  providers: [CursosService]
 })
 export class CursosComponent implements OnInit {
 
@@ -15,6 +16,10 @@ export class CursosComponent implements OnInit {
 
   ngOnInit() {
     this.cursos = this.cursoService.getCursos();
+
+    CursosService.criouNovoCurso.subscribe(
+      curso => this.cursos.push(curso)
+    );
   }
 
 }
