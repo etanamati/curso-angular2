85712a44eaa18f01c47a262eb55c205f5ac68023
Elton Tanamati <elton.tanamati@elotech.localnet>
2017-11-21 12:48:52 -0200

    Rotas

---
+++ rotas/src/app/app.routing.ts
@@ -0,0 +1,14 @@
+import { CursosComponent } from './cursos/cursos.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { ModuleWithProviders } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+const APP_ROUTES: Routes = [
+  { path: 'cursos', component: CursosComponent },
+  { path: 'login', component: LoginComponent },
+  { path: '', component: HomeComponent }
+
+];
+
+export const routing: ModuleWithProviders = RouterModule.forRoot(APP_ROUTES);
