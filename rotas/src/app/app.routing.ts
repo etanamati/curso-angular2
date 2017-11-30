e68fa9847593df3ed4bacebabec669ae5924ed80
Elton Tanamati <elton.tanamati@elotech.localnet>
2017-11-30 12:52:39 -0200

    Parametros nas rotas

--- rotas/src/app/app.routing.ts
+++ rotas/src/app/app.routing.ts
@@ -1,11 +1,14 @@
-import { CursosComponent } from './cursos/cursos.component';
-import { LoginComponent } from './login/login.component';
-import { HomeComponent } from './home/home.component';
 import { ModuleWithProviders } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { CursoDetalheComponent } from './curso-detalhe/curso-detalhe.component';
+import { CursosComponent } from './cursos/cursos.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+
 const APP_ROUTES: Routes = [
   { path: 'cursos', component: CursosComponent },
+  { path: 'curso/:id', component: CursoDetalheComponent },
   { path: 'login', component: LoginComponent },
   { path: '', component: HomeComponent }
 
